2f9bbc2639f9b0cceccde208a18d9c236e64c9fc
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-08-31 11:03:31 -0600

    getting started: questions and question prompt function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,56 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const { default: inquirer } = require("inquirer");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 
+//npm i inquirer
+console.log('Lets get started')
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    name: "title",
+    message: "What is the Title of your App?"
+},
+{
+    type: 'input',
+    name: "description",
+    message: "What is it's purpose?"
+},
+{
+    type: 'input',
+    name: "use",
+    message: "How do you use it?"
+},
+{
+    type: 'input',
+    name: "install",
+    message: "Are there installation instructions?"
+},
+{
+    type: 'list',
+    name: "license",
+    message: "What is the License of your App?"
+    choices: ['MIT', 'ISC', 'GNUPLv3'],
+    filter (choice) {
+        return choice
+    }
+},
+];
+writeToFile();
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+async function writeToFile() { 
+    return inquirer.prompt(questions);
+}
+      .then((answers)=> {
+            console.log(answers)
+        return answers    
+        })
+    .catch((error)=> {
+        console.error();
+      })   
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
